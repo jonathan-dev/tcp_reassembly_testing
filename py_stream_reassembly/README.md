95639e0311d82d2c4ed534cf9b53fb90b81de050
jonathan <jonathan.drude@gmail.com>
2022-07-14 09:39:35 +0200

    rename of pybindings folder + added documentation

--- py_stream_reassembly/README.md
+++ py_stream_reassembly/README.md
@@ -5,3 +5,7 @@ provides python bindings.
 
 The file `example.py` shows how the library can be imported and used in
 python.
+
+# Development
+This Project has been build with `maturin` which needs to be installed
+via pip. The command `maturin develop` can be used to build the project.
--- py_stream_reassembly/example.py
+++ py_stream_reassembly/example.py
@@ -7,7 +7,7 @@ parser.add_argument('--file', type=str, required=True)
 
 args = parser.parse_args()
 
-for (key, data, inc) in py_bindings.Reass(args.file):
+for (key, data, inc) in py_stream_reassembly.Reass(args.file):
     print( key.src, " -> ", key.dst)
     print(data)
     if inc:
